28b70d75468a70d205accce7aff7174ea322fe43
Michał Kapuściński <kapuchaster@gmail.com>
2023-01-12 12:01:03 +0100

    remove unnecessary React imports

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -6,9 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById("invoice")
 );
 
