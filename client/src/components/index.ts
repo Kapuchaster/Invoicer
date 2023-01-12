9e1da404b31ac0cdeaa33cdbfb1858a54b8061ca
Michał Kapuściński <kapuchaster@gmail.com>
2023-01-12 20:17:37 +0100

    Rename Table to OldTable

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,4 +2,4 @@ export { default as Companies } from "./Companies";
 export { default as Flex } from "./Flex";
 export { default as Header } from "./Header";
 export { default as Logo } from "./Logo";
-export { default as Table } from "./Table";
+export { default as Table } from "./OldTable";
