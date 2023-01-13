b6b69830f5e9a32377f324bacc5cdab9139d736d
Michał Kapuściński <kapuchaster@gmail.com>
2023-01-13 22:22:17 +0100

    Adjust Invoice comopnent containers

--- client/src/Pages/Invoice/index.tsx
+++ client/src/Pages/Invoice/index.tsx
@@ -10,14 +10,20 @@ const Invoice = () => {
   return (
     <>
       <OldTable />
-      <div>--------------------------</div>
-      <Header invoiceId="1234" createdAt="2021-10-11" due="2021-10-11" />
-      <Companies
-        company="Acme, GmbH."
-        fullName="Bob Hans Jens, The Great"
-        email="youknowit@star-wars-is-real.pew"
-      />
-      <Table />
+      <div>----------------------------------------------------</div>
+      <div style={{ padding: "0 0.625rem" }}>
+        <Header invoiceId="1234" createdAt="2021-10-11" due="2021-10-11" />
+        <div style={{ marginTop: "2rem" }}>
+          <Companies
+            company="Acme, GmbH."
+            fullName="Bob Hans Jens, The Great"
+            email="youknowit@star-wars-is-real.pew"
+          />
+        </div>
+      </div>
+      <div style={{ marginTop: "3.5rem" }}>
+        <Table />
+      </div>
       <Summary />
     </>
   );
