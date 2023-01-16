a72d6dd5d03c75f8e9e7b48cb2b643aa43017882
Michał Kapuściński <kapuchaster@gmail.com>
2023-01-16 15:49:27 +0100

    Extend Table Cell with disableTextBreak property

--- client/src/components/molecules/table/Cell.tsx
+++ client/src/components/molecules/table/Cell.tsx
@@ -1,18 +1,27 @@
 interface Props {
   isHeaderCell?: boolean;
   alignRight?: boolean;
+  disableTextBreak?: boolean;
   children: string | JSX.Element;
 }
 /**
  * @param isHeaderCell will wrap content in <th>, else <td>
  * @param alignRight will align content to the right of the cell, default left
+ * @param disableTextBreak disable text breaking 
  */
-const Cell = ({ isHeaderCell, alignRight, children }: Props) => {
+const Cell = ({
+  isHeaderCell,
+  alignRight,
+  disableTextBreak,
+  children,
+}: Props) => {
   const aligment = alignRight ? "right" : "left";
+  const whiteSpace = disableTextBreak ? "nowrap" : "normal";
+
   return isHeaderCell ? (
-    <th style={{ textAlign: aligment }}>{children}</th>
+    <th style={{ textAlign: aligment, whiteSpace: whiteSpace }}>{children}</th>
   ) : (
-    <td style={{ textAlign: aligment }}>{children}</td>
+    <td style={{ textAlign: aligment, whiteSpace: whiteSpace }}>{children}</td>
   );
 };
 
