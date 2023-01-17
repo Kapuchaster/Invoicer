7150a5b671fdb29e959c67ed730a3c2d03db468c
Michał Kapuściński <kapuchaster@gmail.com>
2023-01-17 15:10:39 +0100

    Add DnD functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,43 @@
+import { useCallback, useEffect, useState } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import DndFrame from "./components/organisms/dndTargetBox";
 import { useGetInvoice } from "./hooks/useGetInvoice";
 import { Invoice } from "./pages";
+import { InvoiceType } from "./types";
 
 import "./App.css";
 
-function App() {
+const App = () => {
   const { data, loading, error } = useGetInvoice();
+  const [invoice, setInvoice] = useState<InvoiceType>();
+
+  useEffect(() => {
+    data && setInvoice(data.invoice);
+  }, [data]);
+
+  const handleFileDrop = useCallback(
+    async (file: File) => {
+      if (file) {
+        const dndInvoiceText = await file.text();
+        const dndInvoiceJSON = JSON.parse(dndInvoiceText) as InvoiceType;
+        setInvoice(dndInvoiceJSON);
+      }
+    },
+    [setInvoice]
+  );
 
   if (loading) return <p>Loading</p>;
   if (error) return <p>Data Error</p>;
-  if (!data) return <p>No Data</p>;
+  if (!invoice) return <p>No Data</p>;
 
-  return <Invoice invoice={data.invoice} />;
-}
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <DndFrame onDrop={handleFileDrop}>
+        <Invoice invoice={invoice} />
+      </DndFrame>
+    </DndProvider>
+  );
+};
 
 export default App;
---
+++ client/src/components/organisms/dndTargetBox/index.tsx
@@ -0,0 +1,79 @@
+import type { CSSProperties } from "react";
+import type { DropTargetMonitor } from "react-dnd";
+import { useDrop } from "react-dnd";
+import { NativeTypes } from "react-dnd-html5-backend";
+
+const dragSpaceContainer: CSSProperties = {
+  position: "absolute",
+  display: "flex",
+  left: 0,
+  top: 0,
+  height: "100%",
+  width: "100%",
+  background: "#03055bb0",
+};
+
+const dragSpaceContent: CSSProperties = {
+  display: "flex",
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  border: "3px dotted #3cfaa9",
+  borderRadius: "1rem",
+  margin: "1rem",
+  fontSize: "3rem",
+  color: "white",
+};
+
+const jsonFileType = "application/json";
+
+export interface FrameProps {
+  children: JSX.Element;
+  onDrop: (file: File) => void;
+}
+
+/**
+ * NOTE: This component works only inside <DndProvider from "react-dnd"
+ */
+const DndFrame = ({ children, onDrop }: FrameProps) => {
+  const [{ canDrop }, drop] = useDrop(
+    () => ({
+      accept: [NativeTypes.FILE],
+      drop(item: { files: any[] }) {
+        if (onDrop) {
+          if (item.files.length > 1) {
+            return alert("you can put only 1 file");
+          }
+          if (item.files[0].type !== jsonFileType) {
+            return alert("file must be a json");
+          }
+          onDrop(item.files[0]);
+        }
+      },
+      canDrop() {
+        return true;
+      },
+      collect: (monitor: DropTargetMonitor) => {
+        return {
+          canDrop: monitor.canDrop(),
+        };
+      },
+    }),
+    [onDrop]
+  );
+
+  const isActive = canDrop;
+
+  return (
+    <div>
+      {isActive && (
+        <div ref={drop} style={dragSpaceContainer}>
+          <div style={dragSpaceContent}>Put your JSON file here</div>
+        </div>
+      )}
+      {children}
+    </div>
+  );
+};
+
+export default DndFrame;
